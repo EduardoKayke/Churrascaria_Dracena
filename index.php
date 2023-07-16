f89ba5bec99465d98dec3de39389324df01dbcbe
EduardoKayke <hakuzosu.developer@gmail.com>
2023-07-16 00:14:55 -0300

    Churrascaria Dracena

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <?php
     include_once("./templates/header.php");
 ?>      
-        <main class="main_site">
-            <a href="<?= $BASE_URL ?>" id="logo">
-                <img src="<?= $BASE_URL ?>/image/logo.svg" alt="Churrascaria Dracena">
-            </a>
-            <div class="box_img">
-
+        <main class="main_index">
+            <div class="main_index_content">
+                <h1>Churrascaria Dracena</h1>
+                <h2>33 Anos de Tradição e Sabor Incomparável</h2>
+                <p>
+                    O segredo do sucesso da Churrascaria Dracena está, sem dúvida, na qualidade de suas carnes. Selecionando os melhores cortes e investindo em fornecedores confiáveis, a casa se destaca por servir carnes suculentas e saborosas, preparadas com maestria. O tradicional churrasco brasileiro é a especialidade, mas o restaurante também oferece uma variedade de pratos que agradam a todos os gostos, desde opções de saladas frescas e acompanhamentos até pratos quentes irresistíveis.
+                </p>
+                <p>
+                    Além do churrasco impecável, a Churrascaria Dracena também é reconhecida por sua equipe dedicada de profissionais. Os churrasqueiros são verdadeiros artistas do fogo, dominando técnicas ancestrais para trazer o máximo sabor e suculência às carnes. A equipe de garçons é atenciosa e conhecedora do cardápio, proporcionando uma experiência personalizada a cada cliente que passa por ali.
+                </p>
+                <p>
+                    Outro aspecto que cativa os visitantes é a atmosfera amigável e familiar que permeia todo o ambiente. Muitas famílias e grupos de amigos frequentam o restaurante há gerações, criando memórias afetivas que se misturam com o aroma do churrasco e o som das risadas. É comum ver crianças crescendo ao longo dos anos, continuando a tradição de levar seus filhos e netos à Churrascaria Dracena.
+                </p>
+                <p>
+                    Se você está buscando uma experiência gastronômica memorável, não deixe de conhecer a Churrascaria Dracena. Com sua tradição de 33 anos, ela continuará a encantar paladares e a criar memórias afetivas por muitas décadas ainda. Um verdadeiro refúgio para os amantes da boa comida e da calorosa hospitalidade brasileira.
+                </p>
+                <div class="box_img">
+                    <img src="<?= $BASE_URL ?>source/images/foto23.jpg" alt="Churrascaria Dracena">
+                    <img src="<?= $BASE_URL ?>source/images/foto24.jpg" alt="Churrascaria Dracena">
+                    <img src="<?= $BASE_URL ?>source/images/foto26.jpg" alt="Churrascaria Dracena">
+                </div>
+                <div class="box_img">
+                    <img src="<?= $BASE_URL ?>source/images/foto20.jpg" alt="Churrascaria Dracena">
+                    <img src="<?= $BASE_URL ?>source/images/foto21.jpg" alt="Churrascaria Dracena">
+                    <img src="<?= $BASE_URL ?>source/images/foto17.jpg" alt="Churrascaria Dracena">
+                </div>
             </div>
-            
-            <!--Conteúdo principal-->
-            <h1>Churrascaria Dracena</h1>
-            <h2>50 anos de tradição</h2>
-            <img src="" alt="Churrascaria Dracena">
         </main>
 <?php
     include_once("./templates/footer.php");
