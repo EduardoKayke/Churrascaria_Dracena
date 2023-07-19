5bf57eb93708c26e182e70ecb1d9908fc997ca77
EduardoKayke <hakuzosu.developer@gmail.com>
2023-07-19 00:09:10 -0300

    Churrascaria Dracena

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,13 @@
-    <div class="box_facebook">
-        <a href="https://www.facebook.com/RestauranteDracena/?locale=pt_BR" target="_blank"><img src="<?= $BASE_URL ?>source/icons/icons8-facebook-100.svg" alt="Página Facebook"></a>
-    </div>
-    <footer>
-      <div>
-        <p>Churrascaria Dracena &copy; 2023 | Icons and images by <a target="_blank" href="https://icons8.com">Icons8</a></p>
-      </div>
-    </footer>
-  </body>
+		<footer>
+			<div class="box_facebook">
+				<a href="https://www.facebook.com/RestauranteDracena/?locale=pt_BR" target="_blank"><img src="<?= $BASE_URL ?>source/icons/icons8-facebook-100.svg" alt="Página Facebook"></a>
+			</div>
+			<div>
+				<p>Churrascaria Dracena &copy; 2023 | Icons and images by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+			</div>
+		</footer>
+		<?php
+			include_once("./templates/schema.php");
+		?>
+  	</body>
 </html>
---
+++ templates/schema.php
@@ -0,0 +1,22 @@
+<script defer type="application/ld+json">
+            {
+                "@context": "http://schema.org",
+                "@type": "Restaurant",
+                "name": "Churrascaria Dracena",
+                "description": "33 Anos de Tradição e Sabor Incomparável",
+                "servesCuisine": "Churrasco, Pratos Brasileiros",
+                "address": {
+                    "@type": "PostalAddress",
+                    "streetAddress": "R. Alfredo Pupi, 717-759",
+                    "addressLocality": "Colombo",
+                    "addressRegion": "PR",
+                    "postalCode": "83413-020",
+                    "addressCountry": "Brasil"
+                },
+                "telephone": "(41)3675-7336",
+                "url": "URL do site da churrascaria",
+                "openingHours": "Seg-Sex 07:00-18:00, Sáb-Dom 09:00-16:00",
+                "priceRange": "$",
+                "image": "https://lh3.googleusercontent.com/p/AF1QipPNcQVCdMpra2z15DBuHYStdhuP8ZgtU4N0cMqC=s1360-w1360-h1020"
+            }
+        </script>
