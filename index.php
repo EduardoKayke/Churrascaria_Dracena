82052908e43ab4b2af9e92b1f8474580cadafc6d
EduardoKayke <hakuzosu.developer@gmail.com>
2023-07-18 22:57:47 -0300

    Churrascaria Dracena

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
                     <div class="box_text_flavor">
                         <h2>Sabor</h2>
                         <p>
-                            O segredo do sucesso da Churrascaria Dracena está, sem dúvida, na qualidade de suas carnes. Selecionando os melhores cortes e investindo em fornecedores confiáveis, a casa se destaca por servir carnes suculentas e saborosas, preparadas com maestria. O tradicional churrasco brasileiro é a especialidade, mas o restaurante também oferece uma variedade de pratos que agradam a todos os gostos, desde opções de saladas frescas e acompanhamentos até pratos quentes irresistíveis.
+                            O segredo do sucesso da Churrascaria Dracena está, sem dúvida, na qualidade de suas carnes. Selecionando os melhores cortes e investindo em fornecedores confiáveis, a nossa casa se destaca por servir carnes suculentas e saborosas, preparadas com maestria. O tradicional churrasco brasileiro é a especialidade, mas o restaurante também oferece uma variedade de pratos que agradam a todos os gostos, desde opções de saladas frescas e acompanhamentos até pratos quentes irresistíveis.
                         </p>
                     </div>
                 </section>
@@ -26,32 +26,28 @@
                     <div class="box_text_quality">
                         <h2>Qualidade</h2>
                         <p>
-                            Além do churrasco impecável, a Churrascaria Dracena também é reconhecida por sua equipe dedicada de profissionais. Os churrasqueiros são verdadeiros artistas do fogo, dominando técnicas ancestrais para trazer o máximo sabor e suculência às carnes. A equipe de garçons é atenciosa e conhecedora do cardápio, proporcionando uma experiência personalizada a cada cliente que passa por ali.
+                            Além do churrasco impecável, a Churrascaria Dracena também é reconhecida por sua equipe dedicada de profissionais. Os churrasqueiros são verdadeiros artistas do fogo, dominando técnicas culinárias para trazer o máximo de sabor e suculência às carnes. Nossa equipe de garçons é atenciosa e conhecedora do cardápio, proporcionando uma experiência personalizada a cada cliente que passa por aqui.
                         </p>
                     </div>
                 </section>
-                <!-- <section>
-                    <div class="box_img">
-                        <img src="<?= $BASE_URL ?>source/images/foto23.jpg" alt="Sabor">
-                    </div>
-                    <div class="box_text">
+                <section class="box_fraternity">
+                    <img src="<?= $BASE_URL ?>source/images/fraternity.jpg" alt="Imagem de Carne">
+                    <div class="box_text_fraternity">
                         <h2>Fraternidade</h2>
                         <p>
-                            Outro aspecto que cativa os visitantes é a atmosfera amigável e familiar que permeia todo o ambiente. Muitas famílias e grupos de amigos frequentam o restaurante há gerações, criando memórias afetivas que se misturam com o aroma do churrasco e o som das risadas. É comum ver crianças crescendo ao longo dos anos, continuando a tradição de levar seus filhos e netos à Churrascaria Dracena.
+                            Outro aspecto que cativa os visitantes é a atmosfera amigável e familiar que permeia nosso ambiente. Muitas famílias e grupos de amigos frequentam o restaurante há gerações, criando memórias afetivas que se misturam com o aroma do churrasco e o som das risadas. É comum ver crianças crescendo ao longo dos anos, continuando a tradição de levar seus filhos à Churrascaria Dracena.
                         </p>
                     </div>
                 </section>
-                <section>
-                    <div   div class="box_img">
-                        <img src="<?= $BASE_URL ?>source/images/foto23.jpg" alt="Sabor">
-                    </div>
-                    <div class="box_text">
+                <section class="box_tradition">
+                    <img src="<?= $BASE_URL ?>source/images/tradition1.jpg" alt="Sabor">
+                    <div class="box_text_tradition">
                         <h2>Tradição</h2>
                         <p>
-                            Se você está buscando uma experiência gastronômica memorável, não deixe de conhecer a Churrascaria Dracena. Com sua tradição de 33 anos, ela continuará a encantar paladares e a criar memórias afetivas por muitas décadas ainda. Um verdadeiro refúgio para os amantes da boa comida e da calorosa hospitalidade brasileira.
+                            Se você está buscando uma experiência gastronômica memorável, não deixe de conhecer a Churrascaria Dracena. Com nossa tradição de 33 anos, continuaremos a encantar paladares e a criar memórias afetivas por muitas décadas. Um verdadeiro refúgio para os amantes da boa comida e da calorosa hospitalidade brasileira.
                         </p>
                     </div>
-                </section> -->
+                </section>
             </article>
         </main>
 <?php
